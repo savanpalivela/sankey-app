ee8cb75ec3ff2427a668dff5657c6563b62e96ac
Savan Palivela <savan@Chitti.local>
2020-06-13 20:37:19 +0530

    final changes

--- app/containers/Home/api.js
+++ app/containers/Home/api.js
@@ -2,25 +2,16 @@ const URL = 'https://5ee2faa18b27f300160951b4.mockapi.io/api/data/';
 
 const fetchSankeyData = async () => {
   const response = await Promise.all([
-    fetch(`${URL}nodes`),
-    fetch(`${URL}links`),
     fetch(`${URL}income`),
     fetch(`${URL}expense`),
   ]);
   // if (response[0].status >= 400 || response[1].status >= 400) {
   //   throw new Error(data.errors);
   // }
-  const data = await Promise.all([
-    response[0].json(),
-    response[1].json(),
-    response[2].json(),
-    response[3].json(),
-  ]);
+  const data = await Promise.all([response[0].json(), response[1].json()]);
   return {
-    nodes: data[0],
-    links: data[1],
-    income: data[2],
-    expense: data[3],
+    income: data[0],
+    expense: data[1],
   };
 };
 
